443538d9cb3f6e504a5b3561b0f99da4df1569b5
irwin7 <ravshankhan777@icloud.com>
2021-08-25 14:22:18 +0500

    update

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$('.comments-list').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  arrows: false,
+  dots: true,
+});
+$('.news-list').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+});
